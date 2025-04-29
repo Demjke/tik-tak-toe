1822bc19ad944b4470ae94df99174962c8b7f4c4
Vitaliy Antipov <demjke64@gmail.com>
2025-04-29 19:14:21 +0400

    deploy

--- src/components/Game/Square.jsx
+++ src/components/Game/Square.jsx
@@ -1,7 +1,14 @@
-const Square = ({ value, onClick }) => (
-    <button className="game-board__square square" onClick={onClick}>
-        {value}
-    </button>
-);
+import TakImg from "../../assets/images/tak.png";
+import TikImg from "../../assets/images/tik.png";
+
+const Square = ({ value, onClick }) => {
+    const squareImg = value === "X" ? TikImg : value === "0" ? TakImg : null;
+
+    return (
+        <button className="game-board__square square" onClick={onClick}>
+            {squareImg && <img src={squareImg} alt="" />}
+        </button>
+    );
+};
 
 export default Square;
--- src/components/Game/UserInfo.jsx
+++ src/components/Game/UserInfo.jsx
@@ -1,5 +1,18 @@
 const UserInfo = ({ user, winner }) => (
-    <div className="game-username">{winner ? `Winner: ${winner}` : `User ${user ? "X" : "0"}`}</div>
+    <div className="game-username">
+        {winner ? (
+            <span style={{ color: winner === "X" ? "rgb(245, 77, 98)" : "rgb(135, 228, 58)" }}>Winner: {winner}</span>
+        ) : (
+            <>
+                User
+                {user ? (
+                    <span style={{ color: "rgb(245, 77, 98)" }}> X</span>
+                ) : (
+                    <span style={{ color: "rgb(135, 228, 58)" }}> O</span>
+                )}
+            </>
+        )}
+    </div>
 );
 
 export default UserInfo;
